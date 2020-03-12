4ed6df76a03cea1fa5e01a74b7a9b6eb8a1d5b34
mate <matemislov90@gmail.com>
2020-03-12 10:03:05 +0100

    Tags manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,21 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-TJR229D",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
     // `gatsby-transformer-sharp`,
     // `gatsby-plugin-sharp`,
     // {
